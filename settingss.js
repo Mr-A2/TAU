a18586eb0ee676d938828fb443d57b98bb3cd981
Mr-A2 <A2lateef3@gmail.com>
2025-08-02 09:34:30 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~0itUXCZD#DyDJYjoBoF-yyqO-4Ryct2hd0kkCadxfptwaYD3r9N4",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "9038742280", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
